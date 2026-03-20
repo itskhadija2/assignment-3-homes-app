c287004856882c247cf8714fc2c5bf60f455c1fb
itskhadija2 <khadija.ibrahim@ndus.edu>
2026-03-20 09:41:49 -0500

    Added routing to the angular application

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { Home } from './home/home';
+import { home, Home } from './home/home';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Home],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class App {}
+export class App {
+  title= 'assigment -3- homes-app'}
