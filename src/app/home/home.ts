e3b1002120d6d3e060da6160830124b2a1b5cafb
itskhadija2 <khadija.ibrahim@ndus.edu>
2026-03-20 00:32:41 -0500

    Completed Creating HousingLocation Component and adding it to the Angular app

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from '../housing-location/housing-location';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [HousingLocation], // include component here
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
